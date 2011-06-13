19144f96196462755a581c537681b83f64fe0626
thewanderingbrit <thewanderingbrit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-13 17:50:51 +0000

    Fixed bad image reference
    git-svn-id: https://plugins.svn.wordpress.org/soup-show-off-upcoming-posts/tags/1.3.3@396812 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- soup-upcoming-posts-widget.php
+++ soup-upcoming-posts-widget.php
@@ -75,7 +75,7 @@ License: GPL2
 			</p>
 			<p>
 				<a href="<?php bloginfo('rss2_url') ?>" title="Subscribe to <?php bloginfo('name') ?>">
-					<img style="vertical-align:middle; margin:0 10px 0 0;" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/soup-upcoming-post/icons/rss.png" width="16px" height="16px" alt="Subscribe to <?php bloginfo('name') ?>" />		
+					<img style="vertical-align:middle; margin:0 10px 0 0;" src="<?php bloginfo('wpurl') ?>/wp-content/plugins/soup-show-off-upcoming-posts/icons/rss.png" width="16px" height="16px" alt="Subscribe to <?php bloginfo('name') ?>" />		
 				</a>
 				Don't miss it - <strong><a href="<?php bloginfo('rss2_url') ?>" title="Subscribe to <?php bloginfo('name') ?>">Subscribe by RSS.</a></strong>
 			</p>
