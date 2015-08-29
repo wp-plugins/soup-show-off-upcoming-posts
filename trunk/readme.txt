08e544b11b779c0f80917a17d84b7282f460b6fd
thewanderingbrit <thewanderingbrit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-29 13:43:33 +0000

    Tag 1.10
    git-svn-id: https://plugins.svn.wordpress.org/soup-show-off-upcoming-posts/tags/1.10@1233696 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,211 @@
+=== SOUP - Show off Upcoming Posts ===
+Contributors: thewanderingbrit
+Donate link: https://www.theukedge.com/donate/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=donate
+Tags: upcoming, posts, future, scheduled, widget, sidebar, list, number, title, interest, readers, newsletter, rss feed, feedburner, drafts, soup
+Requires at least: 2.9
+Tested up to: 4.3
+Stable tag: 1.10
+License: GPLv2
+
+Displays your upcoming posts in a sidebar widget to tease your readers
+
+== Description ==
+
+**Like this plugin?** Consider [leaving a quick review](https://wordpress.org/support/view/plugin-reviews/soup-show-off-upcoming-posts "Review SOUP") or writing about how you've used it on your site - [send me a link](https://www.theukedge.com/contact/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=contact "Contact Dave") to that write up if you do.
+
+This plugin is maintained on [GitHub](https://github.com/theukedge/soup-show-off-upcoming-posts), so feel free to use the repository for reporting issues, submitting feature requests and submitting pull requests.
+
+SOUP creates a widget in your sidebar which allows you to display your upcoming posts (scheduled, drafts, or both) to your readers. The idea is to entice your readers to come back and read the article once it gets published, or better yet, subscribe to your RSS feed and/or newsletter.
+
+Here are the configurable options for the widget:
+
+* Title of sidebar widget
+* Number of upcoming posts to display (always in ascending order - newest first)
+* Choose whether to show date of upcoming post
+* Which categories to display posts from
+* Choose whether to show scheduled posts, drafts, or both.
+* Choose which post types to include
+* Display order of the posts (next post first or random order)
+* Message to display for when there are no scheduled posts or drafts
+* Show/hide newsletter link
+* Include link to sign up to newsletter
+
+The plugin has been translated to Spanish (es_ES - thanks to Andrew Curtis) and Farsi (fa_IR - thanks to Saeed Adel Mehraban).
+
+I've got plans to continue developing and updating this plugin. If you have any suggestions on revisions that you'd like to see made, please [get in touch](https://www.theukedge.com/contact/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=contact "Contact Dave") or [find me on Twitter](http://www.twitter.com/daclements "Dave on Twitter").
+
+I also run [Do It With WordPress](https://www.doitwithwp.com/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=my-other-sites "WordPress Tutorials"), which has an array of tutorials for managing, modifying and maintaining your WordPress sites, as well as [The WP Butler](https://www.thewpbutler.com/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=wordpress-services "WordPress Maintenance Services"), a service for keeping your site maintained, backed up, updated and secure.
+
+== Installation ==
+
+1. Upload `soup-show-off-upcoming-posts` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Go to Appearance > Widgets to put the widget in your selected sidebar.
+1. Set your settings, hit Save and go to your site to see it in action.
+
+== Frequently Asked Questions ==
+
+= Are there more features planned in future? =
+
+Absolutely. I'll be providing more options and upgrades in the near future. You can stay up to date by following me on [Twitter](http://www.twitter.com/daclements "Dave on Twitter"). If you have an idea for a new feature, you can create a new feature request on [GitHub](https://github.com/theukedge/soup-show-off-upcoming-posts/issues). If you don't have a GitHub account, then [tell me](https://www.theukedge.com/contact/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=contact "Contact The UK Edge") what this widget should do that it doesn't currently.
+
+= I don't have a newsletter. Is this functionality lost on me? =
+
+Many WordPress users send their RSS feed through FeedBurner, which has the option to deliver your latest posts by email to anyone who subscribes. Just log in to FeedBurner, go to the Publicize tab and select Email Subscriptions where you'll be able to grab the link.
+
+= I'm having issues. What do I do? =
+
+The best thing to do is to submit an issue on [GitHub](https://github.com/theukedge/soup-show-off-upcoming-posts/issues). If you don't have a GitHub account, then [get in touch with me](https://www.theukedge.com/contact/?utm_source=wordpress.org&utm_medium=plugin&utm_campaign=contact "Contact The UK Edge") and I'll see about getting it fixed.
+
+== Screenshots ==
+
+1. An example of the widget in use.
+2. The widget control panel.
+
+== Changelog ==
+
+= 1.10 =
+
+Release date: August 29, 2015
+
+* Added ability to show date of upcoming posts next to post title
+
+= 1.9 =
+
+Release date: August 13, 2015
+
+* Added ability to show upcoming posts for multiple post types, including custom post types
+
+= 1.8 =
+
+Release date: August 12, 2015
+
+* Updated PHP constructor methods to remove deprecated methods
+* Remove author credit
+
+= 1.7.2 =
+
+Release date: October 21, 2014
+
+* Added Spanish translation - props to Andrew Kurtis
+* Correct versioning mistakes in 1.7.1
+
+= 1.7.1 =
+
+Release date: October 21, 2014
+
+* Added Spanish translation - props to Andrew Kurtis
+
+= 1.7 =
+
+Release date: October 8, 2014
+
+* Added option to enable/disable RSS link
+* Minor bug fixes
+
+= 1.6.3 =
+* Made final preparations for localization. Translators: please send me your translations.
+
+= 1.6.2 =
+* Fixed fatal error during plugin activation.
+
+= 1.6.1 =
+* Readied plugin for internationalization, including loading translation files, when they're available.
+* Minor bugfixes.
+
+= 1.6 =
+* Added ability to filter posts by category, so that you can be specific about the particular categories you want to show posts from.
+* Audited and tidied up the code.
+* Prepared plugin for full internationalization - please send translations if you have some.
+
+= 1.5.1 =
+* Modified database query to exclude sticky posts, which were showing up no matter what options you set.
+
+= 1.5 =
+* Added option (and default setting) to configure message that is displayed if there are no posts matching your settings.
+
+= 1.4 =
+* Added option to change display order of posts (random, or next post first)
+
+= 1.3.3 =
+* Fixed bad image link for RSS icon.
+
+= 1.3.2 =
+* Added option to give plugin author credit
+
+= 1.3.1 =
+* Minor updates
+
+= 1.3 =
+* Included option to display either drafts, scheduled posts, or both.
+
+= 1.2 =
+* Included option to display URL to newsletter signup.
+* Revised and tidied up code
+
+= 1.1 =
+* Made changes to include cross-compatibility for different sites.
+
+= 1.0 =
+* Initial version.
+
+== Upgrade Notice ==
+
+= 1.10 =
+* Added ability to show scheduled post date with post title
+
+= 1.9 =
+* Added ability to include multiple/custom post types
+
+= 1.8 =
+* Updated coding standards
+* Remove author credit
+
+= 1.7.2 =
+* Added Spanish translation - props to Andrew Kurtis
+* Correct versioning mistakes in 1.7.1
+
+= 1.7.1 =
+* Added Spanish translation (es_ES) thanks to Andrew Kurtis.
+
+= 1.7 =
+* Added option to enable/disable RSS link. Minor bug fixes.
+
+= 1.6.3 =
+* Made final adjustments for localization.
+
+= 1.6.2 =
+* Fixed fatal error during upgrade and activation.
+
+= 1.6.1 =
+* Minor bug fixes and further localization tweaks.
+
+= 1.6 =
+* Add ability to set posts by category, cleaned up code and prepared widget for complete localization.
+
+= 1.5.1 =
+* Removed sticky posts from showing up in widget.
+
+= 1.5 =
+* Added configurable message for when there are no posts to display
+
+= 1.4 =
+* Added option to change display order of posts (random or next post first)
+
+= 1.3.3 =
+* Adds link to newsletter and provides choice to display drafts, scheduled posts, or both. Included option to give plugin author credit. Fixed incorrect image reference.
+
+= 1.3.2 =
+* Adds link to newsletter and provides choice to display drafts, scheduled posts, or both. Included option to give plugin author credit.
+
+= 1.3.1 =
+* Adds link to newsletter and provides choice to display drafts, scheduled posts, or both.
+
+= 1.3 =
+* Adds link to newsletter and provides choice to display drafts, scheduled posts, or both.
+
+= 1.2 =
+* Adds the ability to also send readers to your newsletter signup page to subscribe to future updates.
+
+= 1.1 =
+Initial version for other sites.
---
+++ trunk/soup-upcoming-posts-widget.php
@@ -0,0 +1,215 @@
+<?php
+/*
+Plugin Name: SOUP - Show Off Upcoming Posts
+Plugin URI: https://github.com/theukedge/soup-show-off-upcoming-posts
+Description: Displays your upcoming posts to tease your readers
+Version: 1.10
+Author: Dave Clements
+Author URI: https://www.theukedge.com
+License: GPLv2
+*/
+
+/*  Copyright 2015  Dave Clements  (email : https://www.theukedge.com/contact/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* ---------------------------------- *
+ * constants
+ * ---------------------------------- */
+
+if ( !defined( 'SOUP_PLUGIN_DIR' ) ) {
+	define( 'SOUP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( !defined( 'SOUP_PLUGIN_URL' ) ) {
+	define( 'SOUP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+
+// Start class soup_widget //
+
+class soup_widget extends WP_Widget {
+
+	// Constructor //
+
+    function soup_widget() {
+    	load_plugin_textdomain('soup', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+        parent::__construct(false, $name = __('Upcoming Posts', 'soup'), array('description' => __('Displays your upcoming posts to entice your readers', 'soup')) );
+    }
+
+	// Extract Args //
+
+	function widget($args, $instance) {
+		extract( $args );
+		$title			= apply_filters('widget_title', $instance['title']); // the widget title
+		$soupnumber		= $instance['soup_number']; // the number of posts to show
+		$showdate		= $instance['show_date']; // whether or not to show the scheduled post date
+		$showrss		= $instance['show_rss']; // whether or not to show the RSS feed link
+		$soup_cat		= $instance['soup_cat']; // exclude posts from these categories
+		$poststatus		= $instance['post_status']; // the statuses of posts to show
+		$posttypes		= $instance['post_types']; // the type of posts to show
+		$posttypesarray	= explode(',', $posttypes); // array of post types
+		$postorder		= $instance['post_order']; // Display newest first or random order
+		$shownews		= isset($instance['show_newsletter']) ? $instance['show_newsletter'] : false ; // whether or not to show the newsletter link
+		$newsletterurl	= $instance['newsletter_url']; // URL of newsletter signup
+		$noresults		= $instance['no_results']; // Message for when there are no posts to display
+
+	// Before widget //
+
+		echo $before_widget;
+
+	// Title of widget //
+
+		if ( $title ) { echo $before_title . $title . $after_title; }
+
+	// Widget output //
+
+		?>
+		<ul class="no-bullets">
+		<?php
+			global $post;
+			$tmp_post = $post;
+			$args = array( 'numberposts' => $soupnumber, 'no_paging' => '1', 'post_status' => $poststatus, 'order' => 'ASC', 'orderby' => $postorder, 'ignore_sticky_posts' => '1', 'category' => $soup_cat, 'post_type' => $posttypesarray );
+			$myposts = get_posts( $args );
+			foreach( $myposts as $post ) : setup_postdata($post); ?>
+				<li>
+					<?php the_title(); ?>
+					<?php if($showdate) {
+						echo '(' . get_the_time( get_option( 'date_format' ) ) . ')';
+					} ?>
+				</li>
+			<?php endforeach; ?>
+			<?php $post = $tmp_post; ?>
+		</ul>
+
+		<?php if (!$myposts) {
+			echo $noresults;
+		} ?>
+
+		<?php if ($showrss) { ?>
+		<p>
+			<a href="<?php bloginfo('rss2_url') ?>" title="<?php _e('Subscribe to ', 'soup'); bloginfo('name'); ?>">
+				<img style="vertical-align:middle; margin:0 10px 0 0;" src="<?php echo plugins_url( 'includes/images/rss.png' , __FILE__ ); ?>" width="16px" height="16px" alt="<?php _e('Subscribe to ', 'soup'); bloginfo('name'); ?>" />
+			</a>
+			<?php _e('Don\'t miss it', 'soup'); ?> - <strong><a href="<?php bloginfo('rss2_url') ?>" title="<?php _e('Subscribe to ', 'soup'); bloginfo('name'); ?>"><?php _e('Subscribe by RSS', 'soup'); ?>.</a></strong>
+		</p>
+		<?php } ?>
+
+		<?php if ($shownews) { ?>
+		<p>
+			<?php _e('Or, just', 'soup'); ?> <strong><a href="<?php echo $newsletterurl; ?>" title="<?php _e('Subscribe to ', 'soup'); bloginfo ('name'); _e(' newsletter', 'soup'); ?>"><?php _e('subscribe to the newsletter', 'soup'); ?></a></strong>!
+		</p>
+		<?php }
+
+	// After widget //
+
+		echo $after_widget;
+	}
+
+	// Update Settings //
+
+	function update($new_instance, $old_instance) {
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['soup_number'] = strip_tags($new_instance['soup_number']);
+		$instance['show_date'] = strip_tags($new_instance['show_date']);
+		$instance['show_rss'] = strip_tags($new_instance['show_rss']);
+		$instance['soup_cat'] = strip_tags($new_instance['soup_cat']);
+		$instance['post_status'] = strip_tags($new_instance['post_status']);
+		$instance['post_types'] = strip_tags($new_instance['post_types']);
+		$instance['post_order'] = strip_tags($new_instance['post_order']);
+		$instance['show_newsletter'] = strip_tags($new_instance['show_newsletter']);
+		$instance['newsletter_url'] = strip_tags($new_instance['newsletter_url'],'<a>');
+		$instance['no_results'] = strip_tags($new_instance['no_results']);
+		return $instance;
+	}
+
+	// Widget Control Panel //
+
+	function form($instance) {
+
+		$defaults = array(
+			'title' => 'Upcoming Posts',
+			'soup_number' => 3,
+			'show_date' => 'off',
+			'show_rss' => 'off',
+			'soup_cat' => '',
+			'post_status' => 'future',
+			'post_types' => 'post',
+			'post_order' => 'date',
+			'show_newsletter' => 'off',
+			'newsletter_url' => '',
+			'no_results' => 'Sorry - nothing planned yet!',
+		);
+
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'soup'); ?>:</label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>'" type="text" value="<?php echo $instance['title']; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('soup_number'); ?>"><?php _e('Number of upcoming posts to display', 'soup'); ?>:</label>
+			<input class="widefat" id="<?php echo $this->get_field_id('soup_number'); ?>" name="<?php echo $this->get_field_name('soup_number'); ?>" type="text" value="<?php echo $instance['soup_number']; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('show_date'); ?>"><?php _e('Show post date', 'soup'); ?>?</label>
+			<input <?php checked( $instance['show_date'], 'on' ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" type="checkbox" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('show_rss'); ?>"><?php _e('Show RSS link', 'soup'); ?>?</label>
+			<input <?php checked( $instance['show_rss'], 'on' ); ?> id="<?php echo $this->get_field_id( 'show_rss' ); ?>" name="<?php echo $this->get_field_name( 'show_rss' ); ?>" type="checkbox" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('soup_cat'); ?>"><?php _e('Categories to include (comma separated i.e. 2,19,12 - leave blank for all categories)', 'soup'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('soup_cat'); ?>" name="<?php echo $this->get_field_name('soup_cat'); ?>" type="text" value="<?php echo $instance['soup_cat']; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('post_status'); ?>"><?php _e('Post status', 'soup'); ?>:</label>
+			<select id="<?php echo $this->get_field_id('post_status'); ?>" name="<?php echo $this->get_field_name('post_status'); ?>" class="widefat" style="width:100%;">
+				<option value="future,draft" <?php selected('future,draft', $instance['post_status']); ?>><?php _e('Both scheduled posts and drafts', 'soup'); ?></option>
+				<option value="future" <?php selected('future', $instance['post_status']); ?>><?php _e('Scheduled posts only', 'soup'); ?></option>
+				<option value="draft" <?php selected('draft', $instance['post_status']); ?>><?php _e('Drafts only', 'soup'); ?></option>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('post_types'); ?>"><?php _e('Post types to display (comma separated for multiple - e.g. post,page,event)', 'soup'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('post_types'); ?>" name="<?php echo $this->get_field_name('post_types'); ?>" type="text" value="<?php echo $instance['post_types']; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('post_order'); ?>"><?php _e('Sort order', 'soup'); ?>:</label>
+			<select id="<?php echo $this->get_field_id('post_order'); ?>" name="<?php echo $this->get_field_name('post_order'); ?>" class="widefat" style="width:100%;">
+				<option value="date" <?php selected('date', $instance['post_order']); ?>><?php _e('Next post first', 'soup'); ?></option>
+				<option value="rand" <?php selected('rand', $instance['post_order']); ?>><?php _e('Random order', 'soup'); ?></option>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('no_results'); ?>"><?php _e('Message to display for no results', 'soup'); ?>:</label>
+			<input class="widefat" id="<?php echo $this->get_field_id('no_results'); ?>" name="<?php echo $this->get_field_name('no_results'); ?>" type="text" value="<?php echo $instance['no_results']; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('show_newsletter'); ?>"><?php _e('Show Newsletter', 'soup'); ?>?</label>
+			<input <?php checked( $instance['show_newsletter'], 'on' ); ?> id="<?php echo $this->get_field_id( 'show_newsletter' ); ?>" name="<?php echo $this->get_field_name( 'show_newsletter' ); ?>" type="checkbox" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('newsletter_url'); ?>"><?php _e('Newsletter URL', 'soup'); ?>:</label>
+			<input class="widefat" id="<?php echo $this->get_field_id('newsletter_url'); ?>" name="<?php echo $this->get_field_name('newsletter_url'); ?>" type="text" value="<?php echo $instance['newsletter_url']; ?>" />
+		</p>
+    <?php }
+
+}
+
+// End class soup_widget
+
+add_action('widgets_init', create_function('', 'return register_widget("soup_widget");'));
